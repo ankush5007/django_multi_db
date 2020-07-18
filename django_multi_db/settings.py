037e9c601bf8d1c361c2bd18b16938ef2d856768
ankush5007 <ankush.jetly@yahoo.com>
2020-07-18 09:54:28 +0530

    add restframework

--- django_multi_db/settings.py
+++ django_multi_db/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'demo.apps.DemoConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -81,7 +84,13 @@ DATABASES = {
     }
 }
 
-
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
